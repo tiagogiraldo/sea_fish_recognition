dff34a684b8b5a30abd25588b6095967eeeb4f1b
tiagogiraldo <26585484+tiagogiraldo@users.noreply.github.com>
2020-09-14 21:00:07 -0500

    Update streamlit_call.py

--- .binder/streamlit_call.py
+++ .binder/streamlit_call.py
@@ -10,6 +10,5 @@ def load_jupyter_server_extension(nbapp):
             "fish_app.py", 
             "--browser.serverAddress=0.0.0.0", 
             "--server.enableCORS=False",
-            "--runner.fixMatplotlib=True"
         ]
     )
