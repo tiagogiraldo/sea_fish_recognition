89a9c19a528d86f48e741ec9d12d31e1a5af59e5
tiagogiraldo <26585484+tiagogiraldo@users.noreply.github.com>
2020-09-15 20:10:57 -0500

    Updated fish_app

--- .binder/fish_app.py
+++ .binder/fish_app.py
@@ -9,30 +9,30 @@ import json
 
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
-st.title("FISH CLASSIFIER API")
+st.title("FISH SPECIES RECOGNITION API")
 
 introduction_str = 'This is a sea fish classifier.  It was made with 44,134 images corresponding to 44 species, which were scraped, \
     or downloaded manually in some cases. It was done by transfer of learning using the RESNET 34 model from Pytorch and the Fastai2 libraries. \
-    In the validation of the model, the training error was 9%.'
+    In the validation of the model, the training error was 7%.'
 
 st.markdown(introduction_str)
 
 
 # Loading Model
-
-@st.cache(persist=True)
-fish_classifier = load_learner(Path(".",'fish_classification.pkl'))
+path = Path(".")
+#@st.cache(persist=True)
+fish_classifier = load_learner(path/'fish_classification.pkl')
 
 
 # load Wikipedia dictionaries info
-@st.cache(persist=True)
-with open(Path(".",'name_dict.json')) as file1:
+
+with open(path/'name_dict.json') as file1:
     name_dict= json.load(file1)
-@st.cache(persist=True)    
-with open(Path(".",'fish_summaries.json')) as file2:
+    
+with open(path/'fish_summaries.json') as file2:
     fish_summaries= json.load(file2)        
-@st.cache(persist=True)    
-with open(Path(".",'url_dict.json')) as file3:
+    
+with open(path/'url_dict.json') as file3:
     url_dict= json.load(file3)
     
 
@@ -51,7 +51,7 @@ if file_up:
     image = PILImage.create(file_up)
     pred,pred_idx,probs = fish_classifier.predict(image)
 
-    st.subheader("Fish Prediction")
+    st.subheader("Fish Specie Prediction")
     out_label = f'Prediction: {name_dict[pred]};\n\n Probability: {probs[pred_idx]:.03f}'
 
     st.write(out_label)
@@ -63,3 +63,26 @@ if file_up:
     out_url = f"Wikipedia url source: {url_dict[pred]}"
     st.write(out_url)
  
+
+
+    # Wikipedia data
+
+
+
+#from fastai2.vision.all import *
+#from fastai2.vision.widgets import *
+
+
+
+#inferencer = load_learner(path)
+
+#img_bytes = st.file_uploader("Squash It!!", type=['png', 'jpg', 'jpeg'])
+#if img_bytes is not None:
+#    st.write("Image Uploaded Successfully:")
+#    img = PIL.Image.open(img_bytes)
+
+#    pred_class, pred_idx, outputs = inferencer.predict(img)
+#    for out in outputs:
+#        st.write(out)
+
+#    st.write("Decision: ", pred_class)
