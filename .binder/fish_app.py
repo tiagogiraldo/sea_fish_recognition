48679e9e7e46d46200ca0ce7e5c2deab4651d0f6
tiagogiraldo <26585484+tiagogiraldo@users.noreply.github.com>
2020-09-15 19:22:28 -0500

    Update fish_app.py

--- .binder/fish_app.py
+++ .binder/fish_app.py
@@ -21,18 +21,18 @@ st.markdown(introduction_str)
 # Loading Model
 
 @st.cache(persist=True)
-fish_classifier = load_learner(Path('~/fish_classification.pkl'))
+fish_classifier = load_learner(Path(".",'fish_classification.pkl'))
 
 
 # load Wikipedia dictionaries info
 @st.cache(persist=True)
-with open(Path('~/name_dict.json')) as file1:
+with open(Path(".",'name_dict.json')) as file1:
     name_dict= json.load(file1)
 @st.cache(persist=True)    
-with open(Path('~/fish_summaries.json')) as file2:
+with open(Path(".",'fish_summaries.json')) as file2:
     fish_summaries= json.load(file2)        
 @st.cache(persist=True)    
-with open(Path('~/url_dict.json')) as file3:
+with open(Path(".",'url_dict.json')) as file3:
     url_dict= json.load(file3)
     
 
@@ -56,7 +56,7 @@ if file_up:
 
     st.write(out_label)
     st.markdown("\n\n")
-    st.subheader("Wikipedia Information About Fish Classificated")
+    st.subheader("Wikipedia Information About Classified Fish")
     st.markdown("\n")
     st.write(fish_summaries[pred])
     st.markdown("\n")
